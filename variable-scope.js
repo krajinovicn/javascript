6daaa88ab9c818e37995b252c5cb733efd2f91c6
Nikola Krajinovic <nikolakrajinovic@Nikolas-MacBook-Pro.local>
2022-02-22 11:35:14 +0100

    variable scope

---
+++ variable-scope.js
@@ -0,0 +1,16 @@
+console.log("variable-scope script loaded");
+
+let z = 3;
+function foo () {
+    for (let i = 0; i < 5; i++) {
+        var x = 1;
+        let y = 2;
+    }
+    console.log(x); // success
+    // console.log(y); // ReferenceError - must be a var
+    console.log(z); // success no matter the declaration type
+    var t = 4;
+}
+
+foo();
+// console.log(t); // ReferenceError no matter the declaration type
